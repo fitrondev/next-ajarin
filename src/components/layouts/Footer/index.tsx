2e1ba1fe94676e64e376d0611ed410eaf173c1f5
fitronansori <fitronansori7@gmail.com>
2025-05-09 20:18:26 +0800

    Refactor Footer component to improve layout and update social media links with new styles

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="bg-white dark:bg-gray-900">
       <div className="container pt-16 pb-6">
         <div className="lg:flex lg:items-center lg:justify-between">
-          <div className="flex justify-center">
+          <div className="flex justify-center md:justify-start">
             <Logo />
           </div>
 
@@ -195,8 +195,8 @@ const Footer = () => {
                   </span>
 
                   <span className="relative flex size-2">
-                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-teal-400 opacity-75"></span>
-                    <span className="relative inline-flex size-2 rounded-full bg-teal-500"></span>
+                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-600 opacity-75"></span>
+                    <span className="relative inline-flex size-2 rounded-full bg-primary"></span>
                   </span>
                 </Link>
               </li>
@@ -238,11 +238,11 @@ const Footer = () => {
 
           <ul className="mt-4 flex justify-center gap-6 sm:mt-0 sm:justify-start">
             <li>
-              <a
+              <Link
                 href="#"
                 rel="noreferrer"
                 target="_blank"
-                className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75"
+                className="text-primary transition hover:text-primary/75 "
               >
                 <span className="sr-only">Facebook</span>
                 <svg
@@ -257,15 +257,15 @@ const Footer = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 href="#"
                 rel="noreferrer"
                 target="_blank"
-                className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75"
+                className="text-primary transition hover:text-primary/75"
               >
                 <span className="sr-only">Instagram</span>
                 <svg
@@ -280,15 +280,15 @@ const Footer = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 href="#"
                 rel="noreferrer"
                 target="_blank"
-                className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75"
+                className="text-primary transition hover:text-primary/75"
               >
                 <span className="sr-only">Twitter</span>
                 <svg
@@ -299,15 +299,15 @@ const Footer = () => {
                 >
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                 </svg>
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 href="#"
                 rel="noreferrer"
                 target="_blank"
-                className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75"
+                className="text-primary transition hover:text-primary/75"
               >
                 <span className="sr-only">GitHub</span>
                 <svg
@@ -322,15 +322,15 @@ const Footer = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 href="#"
                 rel="noreferrer"
                 target="_blank"
-                className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75"
+                className="text-primary transition hover:text-primary/75"
               >
                 <span className="sr-only">Dribbble</span>
                 <svg
@@ -345,7 +345,7 @@ const Footer = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
